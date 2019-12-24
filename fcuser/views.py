2150e43ee444e4b352d6c07b2d81fbd2a6935bec
Alex <dnjscksdn98@gmail.com>
2019-12-24 18:37:13 +0900

    회원가입 클래스 기반 View 추가

--- fcuser/views.py
+++ fcuser/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+class RegisterView(FormView):  # 클래스 기반 View를 사용해서 코드 단축
+    template_name = "register.html"
+    form_class = RegisterForm
+    success_url = "/"  # 성공시 해당 url로 이동
