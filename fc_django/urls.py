5b6dbe1335c53aaf18a3b2dc34feffa09e9b2639
Alex <dnjscksdn98@gmail.com>
2019-12-26 17:29:15 +0900

    로그인, 상품리스트, 상품등록 url 추가

--- fc_django/urls.py
+++ fc_django/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from fcuser.views import index, RegisterView, LoginView
+from product.views import ProductList, ProductCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
     path("register/", RegisterView.as_view()),  # 클래스일 경우 뒤에 as_view()를 붙이기
-    path("login/", LoginView.as_view())
+    path("login/", LoginView.as_view()),
+    path("product/", ProductList.as_view()),
+    path("product/create/", ProductCreate.as_view())
 ]
