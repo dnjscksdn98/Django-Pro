e4334e231fdeeeecee85a50c4a431625515dbad8
Alex <dnjscksdn98@gmail.com>
2019-12-26 17:28:12 +0900

    humanize 앱 활성화

--- fc_django/settings.py
+++ fc_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',  # humanize 앱 활성화
     'fcuser',
     'product',
     'order',
