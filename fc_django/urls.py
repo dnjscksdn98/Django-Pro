d0dc482d493947faa9681511d80460608847882a
Alex <dnjscksdn98@gmail.com>
2019-12-28 19:38:37 +0900

    주문하기 & 주문조회 url 추가

--- fc_django/urls.py
+++ fc_django/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from fcuser.views import index, RegisterView, LoginView
 from product.views import ProductList, ProductCreate, ProductDetail
+from order.views import OrderCreate, OrderList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,7 @@ urlpatterns = [
     path("login/", LoginView.as_view()),
     path("product/", ProductList.as_view()),
     path("product/create/", ProductCreate.as_view()),
-    path("product/<int:pk>/", ProductDetail.as_view())
+    path("product/<int:pk>/", ProductDetail.as_view()),
+    path("order/", OrderList.as_view()),
+    path("order/create", OrderCreate.as_view())
 ]
