86e4f99385138fb97b7185192e1bc6f71a4f9fa8
Alex <dnjscksdn98@gmail.com>
2019-12-28 19:46:28 +0900

    상세보기 페이지에서 OrderForm적용을 위한 함수 추가

--- product/views.py
+++ product/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
+from order.forms import OrderForm
 
 
 class ProductList(ListView):
@@ -21,3 +22,10 @@ class ProductDetail(DetailView):
     template_name = "product_detail.html"
     queryset = Product.objects.all()
     context_object_name = "product"
+
+    # 별도로 원하는 데이터를 가져오는 함수(페이지 상세보기에서 OrderForm을 가져오기)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # 2. OrderForm 객체 생성시 request 전달, OrderForm 안에서 request에 접근하기 위해서
+        context["form"] = OrderForm(self.request)
+        return context
