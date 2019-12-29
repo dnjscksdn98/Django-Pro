c10e515891e2de096df2beb90109e84d9812a7b3
Alex <dnjscksdn98@gmail.com>
2019-12-29 16:25:29 +0900

    데코레이터 적용

--- product/views.py
+++ product/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
 from order.forms import OrderForm
+from fcuser.decorator import admin_required
 
 
 class ProductList(ListView):
@@ -12,6 +14,7 @@ class ProductList(ListView):
     context_object_name = "product_list"
 
 
+@method_decorator(admin_required, name="dispatch")  # decorator 적용
 class ProductCreate(FormView):
     template_name = "register_product.html"
     form_class = RegisterForm
