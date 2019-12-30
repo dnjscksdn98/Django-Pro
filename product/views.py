bea4edcda731701211a2dda5b7f69b82d36f576d
Alex <dnjscksdn98@gmail.com>
2019-12-30 16:16:53 +0900

    상품 리스트 API와 상품 상세보기 API 추가

--- product/views.py
+++ product/views.py
@@ -2,10 +2,33 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
+from rest_framework import generics, mixins
+
 from .models import Product
 from .forms import RegisterForm
+from .serializers import ProductSerializer
 from order.forms import OrderForm
 from fcuser.decorator import admin_required
+
+
+class ProductListAPI(generics.GenericAPIView, mixins.ListModelMixin):  # 모든 상품 리스트 보기 API
+    serializer_class = ProductSerializer  # 데이터에 대한 검증을 위해서 serializer 등록
+
+    def get_queryset(self):  # 어떤 데이터를 가져올것인지 알기 위해 get_queryset() 오버라이딩
+        return Product.objects.all().order_by("id")
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class ProductDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin):  # 상품 상세보기 API
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):  # 먼저 모든 데이터를 가져온 이후에 원하는 특정 데이터는 get() 함수에서 가져옴
+        return Product.objects.all().order_by("id")
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class ProductList(ListView):
